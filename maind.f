64bb909c30b9a319caaff36c49acc60ec5fb9adb
Nathan Barnes <npbarnes@alaska.edu>
2015-07-20 10:48:46 -0800

    New debug mode always uses the same seeds.
    To start in debug mode add debug to the command line.
    `mpirun -n 24 ./hybrid debug`
    
    Needs a seeds folder in root directory.

--- maind.f
+++ maind.f
@@ -140,6 +140,7 @@ c      real divu(nx,ny,nz)
       
 c      character filenum
       character(len=:), allocatable::filenum
+      character(len=10) :: arg
 
 
 
@@ -221,8 +222,19 @@ c         endif
 c      enddo
 
       seed = t1 +my_rank*100
-      call random_initialize()
+
+      ! Make sure all ranks are initialized and sychronized before attempting
+      ! to make a system call
       call MPI_BARRIER(MPI_COMM_WORLD,ierr) 
+      
+      call get_command_argument(number=1,value=arg,status=ierr)
+      if(ierr .eq. 0 .and. trim(arg) .eq. "debug") then
+            call debug_random_initialize()
+      else 
+            call random_initialize()
+      endif
+      call MPI_BARRIER(MPI_COMM_WORLD,ierr) 
+
 
       if (.not.(restart)) then
          do 66 i=1,nx
--- misc.f
+++ misc.f
@@ -50,11 +50,48 @@ c----------------------------------------------------------------------
       stop
       endif
 
+
       call random_seed(put=seed)
 
       end SUBROUTINE random_initialize
 c----------------------------------------------------------------------
 
+!----------------------------------------------------------------------
+      SUBROUTINE debug_random_initialize ( )
+!----------------------------------------------------------------------
+      integer, allocatable :: seed(:)
+      integer :: n,istat
+      logical :: ex
+
+      call random_seed(size = n)
+      allocate(seed(n))
+
+      inquire(file="./seeds/"//int_to_str(my_rank)//"_seed",
+     x exist=ex)
+
+
+      if(ex) then
+      open(unit=999,file="./seeds/"//int_to_str(my_rank)//"_seed",
+     x access="stream", form="unformatted", action="read",
+     x status="old", iostat=istat)
+      read(999) seed
+      close(999)
+      call random_seed(put=seed)
+
+      else
+
+      call random_initialize()
+      call random_seed(get=seed)
+      open(unit=998,file="./seeds/"//int_to_str(my_rank)//"_seed",
+     x form="unformatted")
+      write(998) seed
+      close(998)
+
+      endif
+
+
+      end SUBROUTINE debug_random_initialize
+!----------------------------------------------------------------------
       pure function int_to_str(num)
       integer,intent(in) :: num
       character(len=12) :: temp
