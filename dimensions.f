ae70791784e46f39d38a4715c249a7f750f58c0c
Nathan Barnes <npbarnes@alaska.edu>
2016-05-11 11:09:42 -0800

    Move Pluto and fix dimensions for large run.

--- dimensions.f
+++ dimensions.f
@@ -2,11 +2,11 @@
 
 c simulation domain dimensions
 
-      PARAMETER (nx = 220, ny = 159, nz = 13)
+      PARAMETER (nx = 150, ny = 100, nz = 10)
 c particle array dimensions
 
       integer*4 Ni_max, Ni_max_buf
-      PARAMETER (Ni_max = 6000000)
+      PARAMETER (Ni_max = 12000000)
       PARAMETER (Ni_max_buf = Ni_max/10)
 
       END MODULE DIMENSIONS
