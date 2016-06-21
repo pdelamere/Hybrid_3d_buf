8624f1a1037c6bdbdece1b39ebb1ce0fd419c207
Nathan Barnes <npbarnes@alaska.edu>
2016-06-20 16:26:14 -0800

    Add ignore flag and argument checking
    Cherry picked a change from local and then fixed a path to be generic.
    Conflicts:
    	run.sh

--- run.sh
+++ run.sh
@@ -3,15 +3,38 @@
 # Ensures that no data files are overwritten and that the exact version
 # of the hybrid code being used is stored along with its output.
 
+USAGE="Usage: run.sh [-i] <num-proc>\n"
 if [[ $# -ne 1 ]]; then
-    printf "Wrong number of command line aguments.\n"
-    printf "Usage: run.sh <num-proc>\n"
+    if [[ $# -ne 2 ]]; then
+        printf "Wrong number of command line arguments.\n"
+        printf $USAGE
+        exit 10
+    elif [[ $1 -ne "-i" ]]; then
+        printf "Invalid option"
+        printf $USAGE
+        exit 10
+    else # [[ $# -eq 2 && $1 -eq "-i" ]]; then
+        IGNORE=true
+        NUM_PROC=$2
+    fi
+else
+    IGNORE=false
+    NUM_PROC=$1
+fi
+
+re='^[0-9]+$'
+if ! [[ $NUM_PROC =~ $re ]]; then
+    printf "Invalid number of processors"
+    printf $USAGE
     exit 10
 fi
 
 # Check if working directory is clean
 # if not, exit with error message.
-git diff-index --quiet HEAD -- || { printf "Working directory has uncommited changes.\nPlease commit and retry.\n"; exit 1; }
+if [ "$IGNORE" = false ]; then
+    git diff-index --quiet HEAD -- ||\
+        { printf "Working directory has uncommitted changes.\nPlease commit and retry.\n"; exit 1; }
+fi
 
 # Make sure the program is rebuilt correctly.
 printf "Attempting to rebuild the working directory from scratch.\n"
@@ -29,9 +52,12 @@ cp inputs.dat $DATA_FOLDER/inputs.dat || { printf "Error while copying inputs.da
 cp fileShrinker.py $DATA_FOLDER/fileShrinker.py || { printf "Error while copying fileShrinker.py\n"; exit 7; }
 
 # Record the version of the code being used.
-git rev-parse --verify HEAD > $DATA_FOLDER/version.sha1 || { printf "Error getting the git commit sha1.\n"; exit 8; }
+if [ "$IGNORE" = false ]; then
+    git rev-parse --verify HEAD > $DATA_FOLDER/version.sha1 || { printf "Error getting the git commit sha1.\n"; exit 8; }
+fi
 
-# Finally, run the program.
-mpirun -n $1 $DATA_FOLDER/hybrid > $DATA_FOLDER/output 2> $DATA_FOLDER/error &
+# Finally, run the program from the data folder
+cd $DATA_FOLDER
+mpirun -n $NUM_PROC $DATA_FOLDER/hybrid > $DATA_FOLDER/output 2> $DATA_FOLDER/error &
 
 echo Done
