3a90fb5a5f678b2d495b7fa740c0b194d3d5e6c6
Nathan Barnes <npbarnes@alaska.edu>
2017-01-18 12:54:47 -0900

    Set parameters for a close in view of pluto

--- dimensions.f
+++ dimensions.f
@@ -2,7 +2,7 @@
 
 c simulation domain dimensions
 
-      PARAMETER (nx = 100, ny = 80, nz = 10)
+      PARAMETER (nx = 200, ny = 80, nz = 10)
 c particle array dimensions
 
       integer*4 Ni_max, Ni_max_buf
