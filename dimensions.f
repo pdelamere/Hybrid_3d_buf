ba586ebe6c19a8fc2325ffadda5d03981ce0cb83
Nathan Barnes <npbarnes@alaska.edu>
2017-02-06 13:20:45 -0900

    Prep for quick run.
    Lower beta of pickups, lower Ni_Max, lower nx, set inputs.dat

--- dimensions.f
+++ dimensions.f
@@ -2,11 +2,11 @@
 
 c simulation domain dimensions
 
-      PARAMETER (nx = 150, ny = 80, nz = 10)
+      PARAMETER (nx = 100, ny = 80, nz = 10)
 c particle array dimensions
 
       integer*4 Ni_max, Ni_max_buf
-      PARAMETER (Ni_max = 3000000)
+      PARAMETER (Ni_max = 1500000)
       PARAMETER (Ni_max_buf = Ni_max/10)
 
       END MODULE DIMENSIONS
