9c883f5359439f1f9bf7576d0a6b4cacc7884b83
Nathan Barnes <npbarnes@alaska.edu>
2017-02-08 12:27:22 -0900

    Small box setup.

--- dimensions.f
+++ dimensions.f
@@ -2,11 +2,11 @@
 
 c simulation domain dimensions
 
-      PARAMETER (nx = 100, ny = 100, nz = 12)
+      PARAMETER (nx = 50, ny = 75, nz = 12)
 c particle array dimensions
 
       integer*4 Ni_max, Ni_max_buf
       PARAMETER (Ni_max = 1500000)
-      PARAMETER (Ni_max_buf = Ni_max/10)
+      PARAMETER (Ni_max_buf = Ni_max/3)
 
       END MODULE DIMENSIONS
