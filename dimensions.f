f0b611dc844b2898934ed019452a939e6568ca96
Nathan Barnes <npbarnes@alaska.edu>
2017-09-01 12:47:45 -0800

    High resolution params. Same as mega-run.

--- dimensions.f
+++ dimensions.f
@@ -2,11 +2,11 @@
 
 c simulation domain dimensions
 
-      PARAMETER (nx = 100, ny = 80, nz = 12)
+      PARAMETER (nx = 200, ny = 160, nz = 12)
 c particle array dimensions
 
       integer*4 Ni_max, Ni_max_buf
-      PARAMETER (Ni_max = 3000000)
+      PARAMETER (Ni_max = 12000000)
       PARAMETER (Ni_max_buf = Ni_max/10)
 
       END MODULE DIMENSIONS
