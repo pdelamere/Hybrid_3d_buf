0ed8c9af772f549322260cf5ac66e6d60862aa50
Nathan Barnes <npbarnes@alaska.edu>
2017-09-30 16:27:30 -0800

    Settings for new mega runs

--- dimensions.f
+++ dimensions.f
@@ -2,11 +2,11 @@
 
 c simulation domain dimensions
 
-      PARAMETER (nx = 100, ny = 60, nz = 14)
+      PARAMETER (nx = 200, ny = 160, nz = 30)
 c particle array dimensions
 
       integer*4 Ni_max, Ni_max_buf
-      PARAMETER (Ni_max = 7000000)
+      PARAMETER (Ni_max = 60000000)
       PARAMETER (Ni_max_buf = Ni_max/10)
 
       END MODULE DIMENSIONS
