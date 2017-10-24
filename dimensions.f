411b81fbf8c0327e9b1d1fa8bbd379456fecda15
npbarnes <npbarnes@alaska.edu>
2017-10-24 11:54:16 -0800

    mega parameters

--- dimensions.f
+++ dimensions.f
@@ -2,11 +2,11 @@
 
 c simulation domain dimensions
 
-      PARAMETER (nx = 75, ny = 75, nz = 6)
+      PARAMETER (nx = 200, ny = 160, nz = 3)
 c particle array dimensions
 
       integer*4 Ni_max, Ni_max_buf
-      PARAMETER (Ni_max = 4000000 )
+      PARAMETER (Ni_max = 20000000)
       PARAMETER (Ni_max_buf = Ni_max/10)
 
       END MODULE DIMENSIONS
