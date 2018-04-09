1f0a9a8fb2b3026fa7fcb9a9a8944d08122f3340
npbarnes <npbarnes@alaska.edu>
2018-04-09 14:56:28 -0800

    Add self restarting feature

--- gen_batch.py
+++ gen_batch.py
@@ -12,7 +12,18 @@ batch_commands = """
 #SBATCH --mail-type=FAIL
 """
 
-script = """
+restart_preamble = """
+./restart.sh -m "Automatic restart"
+if sacct -j $1 --format=State |grep -q TIMEOUT
+then
+"""
+
+preamble = """
+echo "Submit restart job"
+sbatch -d afternotok:$SLURM_JOBID restart.slurm $SLURM_JOBID >& restart_job_submission
+sleep 1
+set NEWJOB=$(cat restart_job_submission |cut -f 4 -d " ")
+
 echo "Setting ulimits"
 ulimit -s unlimited
 ulimit -f unlimited
@@ -30,16 +41,42 @@ echo "Generate machinefile"
 cd $SLURM_SUBMIT_DIR
 srun -l /bin/hostname | sort -n | awk '{print $2}' > ./nodes.$SLURM_JOB_ID
 echo "Start Hybrid Code"
-mpirun -np $SLURM_NTASKS --machinefile ./nodes.$SLURM_JOB_ID "hybrid" > output 2> error
+"""
+
+job_fmt = 'mpirun -np $SLURM_NTASKS --machinefile ./nodes.$SLURM_JOB_ID hybrid {} > output 2> error'
+job = job_fmt.format('')
+restart_job = job_fmt.format('restart')
+
+conclusion = """
 RESULT=$?
 echo "Removing the machinefile"
 rm ./nodes.$SLURM_JOB_ID
+
+echo "Cancel restart"
+scancel $NEWJOB
 
 echo "Finish batch script"
 exit $RESULT
 """
 
-def gen_batch(filename, jobname, partition, ntasks, tasks_per_node=None, time=None, test=False, cont=False):
+end_restart ="""
+else
+echo "Previous job failed" > failed
+fi
+"""
+
+
+script = (preamble
+        + job
+        + conclusion)
+
+restart_script = (restart_preamble
+        + preamble
+        + restart_job
+        + conclusion
+        + end_restart)
+
+def gen_batch(filename, jobname, partition, ntasks, tasks_per_node=None, time=None, test=False, cont=False, restart_filename=None):
     extra_batch_commands = ""
     if tasks_per_node is not None:
         extra_batch_commands += "#SBATCH --tasks-per-node={}\n".format(tasks_per_node)
@@ -57,6 +94,15 @@ def gen_batch(filename, jobname, partition, ntasks, tasks_per_node=None, time=No
                 + extra_batch_commands
                 + script)
 
+    if restart_filename is not None:
+        with open(restart_filename, mode='w') as f:
+            f.write(shebang
+                    + batch_commands.format(jobname=jobname, partition=partition, ntasks=ntasks)
+                    + extra_batch_commands
+                    + restart_script)
+
+
+
 if __name__ == '__main__':
-    gen_batch('test.slurm', 'pluto', 'debug', 115)
+    gen_batch('test.slurm', 'pluto', 'debug', 115, restart_filename='restart.slurm')
 
