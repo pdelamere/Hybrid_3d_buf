6ffa6bc5b42d4b10790c13121cadb89b46b41982
Nathan Barnes <npbarnes@alaska.edu>
2018-10-11 14:35:11 -0800

    Turn on pluto

--- maind.f
+++ maind.f
@@ -525,7 +525,7 @@ c======================================================================
          call update_np_boundary(np)
          call update_up(vp,np,up)       !up at n+1/2
 
-         !call ionization(np,xp,vp,vp1)
+         call ionization(np,xp,vp,vp1)
 
 
          call curlB(b1,np,aj)
