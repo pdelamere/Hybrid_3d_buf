3f4b3dd6edd3eb889c3e51ba13f357ce41742a16
Nathan Barnes <npbarnes@alaska.edu>
2018-10-11 14:35:43 -0800

    Starting point settings for new medium resolution runs (with E output)

--- dimensions.f
+++ dimensions.f
@@ -3,10 +3,10 @@
 c simulation domain dimensions
       integer nx, ny, nz
 
-      PARAMETER (nx = 10, ny = 15, nz = 6)
+      PARAMETER (nx = 150, ny = 150, nz = 10)
 c particle array dimensions
 
       integer Ni_max
-      PARAMETER (Ni_max = 50000)
+      PARAMETER (Ni_max = 25000000)
 
       END MODULE DIMENSIONS
