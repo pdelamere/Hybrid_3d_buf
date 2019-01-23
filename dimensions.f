d18b09bc6f502b8fd7b78c5a662030dc45a09298
Nathan Barnes <npbarnes@alaska.edu>
2019-01-23 10:32:18 -0900

    1d dimensions

--- dimensions.f
+++ dimensions.f
@@ -3,10 +3,10 @@
 c simulation domain dimensions
       integer nx, ny, nz
 
-      PARAMETER (nx = 150, ny = 150, nz = 10)
+      PARAMETER (nx = 800, ny = 3, nz = 3)
 c particle array dimensions
 
       integer Ni_max
-      PARAMETER (Ni_max = 25000000)
+      PARAMETER (Ni_max = 2500000)
 
       END MODULE DIMENSIONS
