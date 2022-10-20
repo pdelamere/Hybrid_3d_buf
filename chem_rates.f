ee68449c723a4451335e5781b59a5fa0401e6bc4
npbarnes <npbarnes@alaska.edu>
2022-10-20 13:34:39 -0800

    Fix up photoionization and charge exchange

--- chem_rates.f
+++ chem_rates.f
@@ -9,35 +9,31 @@
       contains
 
 c----------------------------------------------------------------------
-      function equilibrium_density(t, r) result(nn)
-          ! gaussian barium release with equilibrium model of
-          ! photoionization
-          real, intent(in) :: t
-          real, intent(in) :: r
+      function gaussian_density(N, t, r) result(nn)
+          ! Assuming an expasion rate given by vth_n
+          real, intent(in) :: N, t, r
           real :: nn
-          real N
-          real A
+          real :: A
 
-          ! N is the number of neutrals available to be ionized
-          N = N0*exp(-t/tau_photo)
-
-          ! We assume that the neutrals are distributed as
-          ! a gaussian.
           A = N/((sqrt(pi)*vth_n*t)**3)
           nn = A*exp(-r**2/((vth_n*t)**2))
-      end function equilibrium_density
+      end function gaussian_density
 
-      function excitation_density(t, r) result(nn)
-          ! gaussian barium release with excitation model of
-          ! photoionization
+      function equilibrium_N(t) result(N)
+          ! total number of neutrals at time t in the equilibrium model
           real, intent(in) :: t
-          real, intent(in) :: r
-          real :: nn
-          real N
-          real A
+          real :: N
+
+          N = N0*exp(-t/tau_photo)
+      end function equilibrium_N
+
+      function excitation_N(t) result(N)
+          ! total number of neutrals at time t in the excitation model
+          real, intent(in) :: t
+          real :: N
           integer i
 
-          ! This loop computes the number of ions at time t in the
+          ! This loop computes the number of *ions* at time t in the
           ! excitation model
           N = 0
           do i=1,4
@@ -45,44 +41,46 @@ c----------------------------------------------------------------------
           enddo
           ! The number of neutrals is N0 - Ni
           N = N0 - N
+      end function excitation_N
 
-          ! We assume that the neutrals are distributed as
-          ! a gaussian.
-          A = N/((sqrt(pi)*vth_n*t)**3)
-          nn = A*exp(-r**2/((vth_n*t)**2))
-      end function excitation_density
-
-      function equilibrium_ionization_rate(t, N) result(r)
+      function equilibrium_ionization_rate(t) result(r)
+          ! Ionization rate of neutral micro particles 
+          ! in the equilibrium model. Number of new ions per second
+          ! is N_n * rate
           real :: r
-          real :: t
-          real, optional :: N
-          real :: NN
-          if(present(N)) then
-              NN = N
-          else 
-              NN = N0
-          endif
+          ! t is unused (argument is kept for similarity with 
+          ! excitation model
+          real :: t 
 
-          r = NN/tau_photo * exp(-t/tau_photo)
+          r = 1/tau_photo
       end function equilibrium_ionization_rate
 
-      function excitation_ionization_rate(t, N) result(r)
+      function excitation_ionization_rate(t) result(r)
+          ! Ionization rate of neutral micro particles 
+          ! in the excitation model. Number of new ions per second
+          ! is N_n * rate
           real :: r
           real :: t
           integer :: i
-          real, optional :: N
-          real :: NN
-          if(present(N)) then
-              NN = N
-          else 
-              NN = N0
-          endif
 
           r = 0
           do i=1, 4
-            r = r + N0*vc(i)*lambda(i)*exp(lambda(i)*t)
+            r = r + vc(i)*lambda(i)*exp(lambda(i)*t)
           enddo
       end function excitation_ionization_rate
+
+      !!!!!!!!!!! Make sure these are both using the same model e.g.
+      ! both excitation or both equilibrium
+      function ionization_rate(t) result(r)
+          real t
+          real r
+          r = equilibrium_ionization_rate(t)
+      end function
+      function neutral_N(t) result(N)
+          real t
+          real N
+          N = equilibrium_N(t)
+      end function
 
 c---------------------------------------------------------------------
       real FUNCTION neutral_density(i,j,k)
@@ -95,12 +93,14 @@ c---------------------------------------------------------------------
       real FUNCTION neutral_density_continuous(x,y,z)
       real x,y,z
       real xx,yy,zz
-      real r
+      real N,t,r
+      t = simulated_time
 
       call NC_coords(xx,yy,zz, x,y,z)
-
       r = sqrt(xx**2 + yy**2 + zz**2)
-      neutral_density_continuous = excitation_density(simulated_time, r)
+
+      N = neutral_N(t)
+      neutral_density_continuous=gaussian_density(N,t,r)
 
       return
       end FUNCTION neutral_density_continuous
@@ -111,10 +111,9 @@ c----------------------------------------------------------------------
           real xp(Ni_max,3)
           real vp(Ni_max,3)
           real vp1(Ni_max,3)
-          !call photoionization(np,xp,vp,vp1)
-          call photoionization_switching(np,xp,vp,vp1)
-          !call fake_charge_exchange(xp, vp, vp1)
-          call charge_exchange_ionization(xp,vp,vp1)
+          call photoionization(np,xp,vp,vp1)
+          !call photoionization_switching(np,xp,vp,vp1)
+          !call charge_exchange_ionization(xp,vp,vp1)
       end SUBROUTINE ionization
 
       subroutine photoionization_switching(np,xp,vp,vp1)
@@ -123,8 +122,6 @@ c----------------------------------------------------------------------
       real vp(Ni_max,3)
       real vp1(Ni_max,3)
       real sigma
-      real dsigma
-      dsigma = 1.27 ! km/s Don's expansion rate
       sigma = simulated_time*dsigma
 
       if((mod(procnum, 2) .eq. 0) .or. (3*sigma .gt. qz(nz)/2)) then
@@ -157,8 +154,8 @@ c----------------------------------------------------------------------
       real rnd              !random number
       real n_source,vol_source
       real nnofr       !neutral density vs. r
-      real new_macro   !fractional part indicates probabalisitc particle
       real new_micro
+      integer new_macro 
       
       real Np_total         !total number of ions /s
       real vol         !volume of shell vs. r
@@ -175,64 +172,44 @@ c----------------------------------------------------------------------
       integer i,j,k,l,m
       integer ii,jj,kk,ll
       real N
+      integer ierr
 
       call Neut_Center(cx,cy,cz)
       t = simulated_time
 
-c get source density
-
-      
       vol = dx**3
       l1 = Ni_tot+1
       do i = 2,nx-1
          do j = 2,ny-1
             do k = 2,nz-1
-
-               x = qx(i)-cx
-               y = qy(j)-cy
-               z = gz(k)-cz ! global z
-               r = sqrt(x**2+y**2+z**2)
-             
-               pu_beta_p = 50*b_sw_thermal_H 
+               pu_beta_p = 5*b_sw_thermal_H 
                pu_tag = pluto_photoionize_CH4_tag
 
                N = vol*neutral_density(i,j,k)
-               new_micro = excitation_ionization_rate(t, N)*dt
-               new_macro = new_micro*beta*pu_beta_p
+               new_micro = N * ionization_rate(t) * dt
+               new_macro = nint(new_micro*pu_beta_p)
 
-               do ll = 1,min(nint(new_macro), Ni_max - Ni_tot)
+               if(Ni_tot + new_macro .gt. Ni_max) then
+                   call MPI_ABORT(MPI_COMM_WORLD, ierr, ierr)
+                   stop
+               endif
+
+               do ll = 1,new_macro
                   l = Ni_tot + 1
-                  vp(l,1) = x/simulated_time
-                  vp(l,2) = y/simulated_time
-                  vp(l,3) = z/simulated_time
 
                   xp(l,1) = qx(i) + (pad_ranf()-0.5)*dx_grid(i)
                   xp(l,2) = qy(j) + (pad_ranf()-0.5)*dy_grid(j)
                   xp(l,3) = qz(k) + (pad_ranf()-0.5)*dz_grid(k)
 
-                  !find i on non-uniform 
-                  ii=0
- 26               continue
-                  ii = ii + 1
-                  if (xp(l,1) .gt. qx(ii)) go to 26
-                  ii = ii-1
-                  ijkp(l,1)= ii
+                  call NC_coords(x,y,z, xp(l,1), xp(l,2), xp(l,3))
 
-                  !find j on non-uniform 
-                  jj=0
- 18               continue
-                  jj = jj + 1
-                  if (xp(l,2) .gt. qy(jj)) go to 18
-                  jj = jj-1
-                  ijkp(l,2)= jj
+                  vp(l,1) = x/simulated_time
+                  vp(l,2) = y/simulated_time
+                  vp(l,3) = z/simulated_time
 
-                  !find k on non-uniform 
-                  kk=2
- 15               continue
-                  kk = kk + 1
-                  if (xp(l,3) .gt. qz(kk)) go to 15
-                  kk = kk-1
-                  ijkp(l,3)= kk
+                  ijkp(l,1) = search(xp(l,1), qx)
+                  ijkp(l,2) = search(xp(l,2), qy)
+                  ijkp(l,3) = searchk(xp(l,3), qz)
 
                   mrat(l) = ion_amu/m_pu
                   beta_p(l) = pu_beta_p
@@ -241,65 +218,11 @@ c get source density
                   do m=1,3
                      vp1(l,m) = vp(l,m)
                      ! Add the kinetic energy of the particle
-                     ! (often zero)
                      input_E = input_E + 
      x               0.5*(mion/mrat(l))*(vp(l,m)*km_to_m)**2 /
      x                     (beta*beta_p(l))
                   enddo                     
-
                enddo
-               new_macro = new_macro - nint(new_macro)
-               if (Ni_tot+1 .le. Ni_max) then
-                   if (new_macro .gt. pad_ranf()) then
-                      l = Ni_tot + 1
-                      vp(l,1) = x/simulated_time
-                      vp(l,2) = y/simulated_time
-                      vp(l,3) = z/simulated_time
-                      
-                      xp(l,1) = qx(i) + (pad_ranf()-0.5)*dx_grid(i)
-                      xp(l,2) = qy(j) + (pad_ranf()-0.5)*dy_grid(j)
-                      xp(l,3) = qz(k) + (pad_ranf()-0.5)*dz_grid(k)
-
-
-                      !find i on non-uniform 
-                      ii=0
- 27                   continue
-                      ii = ii + 1
-                      if (xp(l,1) .gt. qx(ii)) go to 27
-                      ii = ii-1
-                      ijkp(l,1)= ii
-
-
-                      !find i on non-uniform 
-                      jj=0
- 17                   continue
-                      jj = jj + 1
-                      if (xp(l,2) .gt. qy(jj)) go to 17
-                      jj = jj-1
-                      ijkp(l,2)= jj                     
-
-                      !find k on non-uniform 
-                      kk=2
- 16                   continue
-                      kk = kk + 1
-                      if (xp(l,3) .gt. qz(kk)) go to 16
-                      kk = kk-1
-                      ijkp(l,3)= kk
-                      
-                      mrat(l) = ion_amu/m_pu
-                      beta_p(l) = pu_beta_p
-                      tags(l) = pu_tag
-                      Ni_tot = l
-                      do m=1,3
-                         vp1(l,m) = vp(l,m)
-                         ! Add the kinetic energy of the particle
-                         ! (often zero)
-                         input_E = input_E + 
-     x                          0.5*(mion/mrat(l))*(vp(l,m)*km_to_m)**2/
-     x                          (beta*beta_p(l))
-                      enddo                     
-                   endif
-               endif
             enddo
          enddo
       enddo
@@ -319,24 +242,25 @@ c----------------------------------------------------------------------
 
       real t
       real sigma
-      real dsigma
       real new_micro   
       integer new_macro   
       real pu_beta_p
       integer ierr
       integer l,m
       real r
+      real N
 
       real cx,cy,cz         !x,y,z coord of neutral cloud center
       call Neut_Center2(cx,cy,cz) ! cz is in local coordinates
 
-      dsigma = 1.27 ! km/s Don's expansion rate
       t = simulated_time
       sigma = t*dsigma
 
-      pu_beta_p = 50*b_sw_thermal_H 
-      new_micro = excitation_ionization_rate(t)*dt
-      new_macro = nint(new_micro*beta*pu_beta_p)
+      pu_beta_p = 5*b_sw_thermal_H 
+
+      N = neutral_N(t)
+      new_micro = N * ionization_rate(t)*dt
+      new_macro = nint(new_micro*pu_beta_p)
 
       if((Ni_tot + new_macro) .gt. Ni_max) then
           call MPI_ABORT(MPI_COMM_WORLD, ierr, ierr)
@@ -390,7 +314,6 @@ c----------------------------------------------------------------------
       Ni_tot = Ni_tot + new_macro
       end subroutine photoionization2
 
-
       SUBROUTINE charge_exchange_ionization(xp,vp,vp1)
           real xp(Ni_max,3)
           real vp(Ni_max,3)
@@ -426,7 +349,8 @@ c----------------------------------------------------------------------
          real nvx,nvy,nvz
          real rvx,rvy,rvz 
          real sigma
-         real chex_inv_tau
+         real chex_rate
+         real chex_num
          real chex_prob
 
          integer m
@@ -449,8 +373,17 @@ c----------------------------------------------------------------------
          elseif(tags(l) .eq. sw_thermal_H_tag) then
             sigma = sigma_Ba_chex
          endif
-         chex_inv_tau = nn*sigma*vrel
-         chex_prob = dt*chex_inv_tau
+         chex_rate = nn*sigma*vrel ! * beta_p(l)
+         chex_num = dt*chex_rate
+         ! Chex_prob is not exactly a probablility. Chex prob is given
+         ! by the predicted number of charge exchange events this
+         ! timestep divided by how many micro particles are represented
+         ! by this macro particle. Then we "charge exchange" the whole
+         ! macro particle if rand() < chex_prob.
+         ! You'd think beta_p would make an appearance for the number of
+         ! micro particles, but since it contributes to the expected
+         ! number of charge exchanges also it ends up canceling out.
+         chex_prob = chex_num ! / beta_p(l)
 
          if (pad_ranf() .lt. chex_prob) then
              ! Remove kinetic energy of the old particle
